363699df299e240f0f0fa96147861957098f1914
Patrick Ofilada <pcofilada@gmail.com>
2016-03-12 10:07:40 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ticketbox
+:bulb: Simple ticketbox app.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
